0fd99b2d16d596a38502d19fe08eb124a32c6512
Haoran Zhang <ansel.zhanghr@gmail.com>
2019-05-31 10:39:14 +0800

    Fix Person3.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -45,7 +45,7 @@ public class Person3 {
 	  //Person 3 put your implementation here
 		String output = "";
 		for (int i = 0; i < input.length(); i++) {
-			output = input[i] + output;
+			output = input.charAt(i) + output;
 		}
 		return output;
 	}
